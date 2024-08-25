fea291aa3beaea7c27c630563181bcfe8d87fba7
Tesliuk Vlad <vvv3410@gmail.com>
2024-08-25 14:24:38 +0300

    done 27

---
+++ lesson_27/hw_27_done.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.alert import Alert
+
+
+driver = webdriver.Chrome()
+
+file_path = 'C:/Users/влад/PycharmProjects/aqa_py_290424/lesson_27/dz.html'
+driver.get(f'file:///{file_path}')
+
+def verify_in_frame(frame_id, input_id, secret_text):
+
+    driver.switch_to.frame(frame_id)
+
+    input_field = driver.find_element(By.ID, input_id)
+    input_field.send_keys(secret_text)
+
+    driver.find_element(By.XPATH, "//button[text()='Перевірити']").click()
+
+    alert = Alert(driver)
+    assert alert.text == "Верифікація пройшла успішно!", f"Перевірка для {frame_id} не пройшла успішно!"
+
+    alert.accept()
+
+    driver.switch_to.default_content()
+
+verify_in_frame('frame1', 'input1', 'Frame1_Secret')
+
+verify_in_frame('frame2', 'input2', 'Frame2_Secret')
+
+driver.quit()
